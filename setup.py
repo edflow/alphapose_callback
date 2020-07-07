6c5dfc5e56cea8d3b2ae98411e4427b3c96441f4
Sandro Braun <supermario94123@gmail.com>
2020-07-07 16:49:08 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,56 @@
+from setuptools import setup, find_packages
+
+# allows to get version via python setup.py --version
+__version__ = "dev"
+
+install_requires = [
+    "pyyaml",
+    "tqdm",
+    "numpy",
+    "pandas",  # for csv dataset and eval pipeline
+    "edflow",
+]
+
+install_full = [  # for extra functionality
+    "black",  # for formatting of code
+    "matplotlib",  # for plot_datum
+]
+install_docs = [  # for building the documentation
+    "sphinx >= 1.4",
+    "sphinx_rtd_theme",
+    "better-apidoc",
+]
+install_test = install_full + [  # for running the tests
+    "pytest",
+    "pytest-cov",
+    "coveralls",
+    "coverage < 5.0",  # TODO pinned dependency of coveralls; see https://github.com/coveralls-clients/coveralls-python/issues/203
+]
+extras_require = {"full": install_full, "test": install_test}
+
+long_description = """Reduce boilerplate code for your ML projects. TensorFlow
+and PyTorch. [Documentation](https://edflow.readthedocs.io/)"""
+
+setup(
+    name="alphapose_callback",
+    version=__version__,
+    description="Pose estimation and PCK calculation callback for edflow callbacks.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/edflow/alphapose_callback",
+    author="Sandro Braun",
+    author_email="sandro.braun@iwr.uni-heidelberg.de",
+    license="MIT",
+    packages=find_packages(),
+    package_data={"": ["*.yaml"]},
+    install_requires=install_requires,
+    extras_require=extras_require,
+    zip_safe=False,
+    scripts=[],
+    python_requires=">=3.6",
+    classifiers=[
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+    ],
+)
---
+++ tests/test_alphapose_callback.py
@@ -0,0 +1,148 @@
+import pytest
+import numpy as np
+import os
+import json
+from alphapose_callback import alphapose_callback
+
+
+def _make_pose_file(dir, name, offset=0):
+    keypoints = np.arange(64).reshape((32, 2)) - offset
+    scores = np.ones((32, 1), dtype=np.float16)
+    keypoint_data = np.concatenate([keypoints, scores], axis=1).ravel()
+    fname = os.path.join(dir, name) + ".json"
+    image_id = "img_01.png"
+
+    pose_data = [
+        {
+            "keypoints": list(keypoint_data),
+            "score": 3.0,
+            "bbox": (0, 0, 128, 128),
+            "category_id": 1,
+            "image_id": image_id,
+            "idx": [0.0],
+        },
+    ]
+    with open(fname, "w") as f:
+        json.dump(pose_data, f)
+    return fname
+
+
+def test_read_pose_file(tmpdir):
+    fname_1 = _make_pose_file(tmpdir, "alphapose_1")
+
+    pose_data = alphapose_callback.read_posefile(fname_1)
+    keypoints = pose_data[0]["keypoints"]
+    assert len(keypoints) == 96
+
+
+def test_pck_from_posefiles(tmpdir):
+    fname_1 = _make_pose_file(tmpdir, "alphapose_1")
+    fname_2 = _make_pose_file(tmpdir, "alphapose_2")
+
+    mean_pck = alphapose_callback.pck_from_posefiles(
+        fname_1, fname_2, distance_threshold=10
+    )
+    assert mean_pck == 1.0
+
+    fname_3 = _make_pose_file(tmpdir, "alphapose_3", offset=10)
+
+    mean_pck = alphapose_callback.pck_from_posefiles(
+        fname_1, fname_3, distance_threshold=10
+    )
+    assert mean_pck == 0.0
+
+
+def test_alphapose_callback(tmpdir):
+    outdir = tmpdir.mkdir("alphapose_outdir")
+    true_poses_file = os.path.join(tmpdir, "true_poses.json")
+    import sys
+
+    python_path = sys.executable
+    config = {
+        "alphapose_callback": {
+            "subprocess_args": [
+                python_path,
+                "tests/run_alphapose.py",
+                "transfer_image",  # indir
+                str(outdir),  # outdir
+            ],
+            "indir": "transfer_image",
+            "outdir": str(outdir),
+        },
+        "alphapose_pck_callback": {
+            "true_poses_file": true_poses_file,
+            "distance_threshold": 10,
+        },
+    }
+    root = tmpdir
+    data_in = {}
+    data_out = {}
+
+    results_file = alphapose_callback.alphapose_callback(
+        root, data_in, data_out, config
+    )
+    assert os.path.isfile(results_file)
+
+
+def test_alphapose_pck_callback(tmpdir):
+    outdir = tmpdir.mkdir("alphapose_outdir")
+    true_poses_file = _make_pose_file(tmpdir, "true_poses")
+    import sys
+
+    python_path = sys.executable
+    config = {
+        "alphapose_callback": {
+            "subprocess_args": [
+                python_path,
+                "tests/run_alphapose.py",
+                "transfer_image",  # indir
+                str(outdir),  # outdir
+            ],
+            "indir": "transfer_image",
+            "outdir": str(outdir),
+        },
+        "alphapose_pck_callback": {
+            "true_poses_file": true_poses_file,
+            "distance_threshold": 10,
+        },
+    }
+    root = tmpdir
+    data_in = {}
+    data_out = {}
+
+    callback_data = alphapose_callback.alphapose_pck_callback(
+        root, data_in, data_out, config
+    )
+    assert callback_data["pck"] == 1.0
+
+
+def test_alphapose_pck_callback_V2(tmpdir):
+    outdir = tmpdir.mkdir("alphapose_outdir")
+    true_poses_file = _make_pose_file(tmpdir, "true_poses", offset=10)
+    import sys
+
+    python_path = sys.executable
+    config = {
+        "alphapose_callback": {
+            "subprocess_args": [
+                python_path,
+                "tests/run_alphapose.py",
+                "transfer_image",  # indir
+                str(outdir),  # outdir
+            ],
+            "indir": "transfer_image",
+            "outdir": str(outdir),
+        },
+        "alphapose_pck_callback": {
+            "true_poses_file": true_poses_file,
+            "distance_threshold": 10,
+        },
+    }
+    root = tmpdir
+    data_in = {}
+    data_out = {}
+
+    callback_data = alphapose_callback.alphapose_pck_callback(
+        root, data_in, data_out, config
+    )
+    assert callback_data["pck"] == 0.0
